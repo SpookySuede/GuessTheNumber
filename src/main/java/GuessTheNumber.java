2280bef717ab4680f51eb340aea02cc5ca0a6c24
SpookySuede <spookysuede@gmail.com>
2022-03-14 10:58:49 -0400

    Added try catch

--- src/main/java/GuessTheNumber.java
+++ src/main/java/GuessTheNumber.java
@@ -6,7 +6,6 @@ public class GuessTheNumber {
         String wrongHigh = "Your guess is too high.";
         String wrongLow = "Your guess is too low";
         String guess = "Take a guess.";
-        String response = "";
         int guessCount = 0;
         int random = ThreadLocalRandom.current().nextInt(1,20);
 
@@ -16,27 +15,31 @@ public class GuessTheNumber {
         System.out.println("Well, " + firstName + ", I am thinking of a number between" +
                 " 1 and 20.\n");
         System.out.println(guess);
-
         int input = Integer.parseInt(fetch.next());
         while(true) {
-            System.out.println(random);
-            if (input == random) {
-                System.out.print("Good job, " + firstName + "! You guessed my number in 3 guesses\n");
-                System.out.println("Would you like to play again? (y or n)");
-                fetch.next();
-                break;
-            } else if (input < random) {
-                System.out.println(wrongLow);
-                guessCount++;
-                input = Integer.parseInt(fetch.next());
-            } else if (input > random) {
-                System.out.println(wrongHigh);
-                guessCount++;
-                input = Integer.parseInt(fetch.next());
-            }
-            if (guessCount == 3) {
-                System.out.print("Game over!");
-                break;
+            try {
+                System.out.println(random);
+                if (input == random) {
+                    System.out.print("Good job, " + firstName + "! You guessed my number in 3 guesses\n");
+                    System.out.println("Would you like to play again? (y or n)");
+                    fetch.next();
+                    break;
+                } else if (input < random) {
+                    System.out.println(wrongLow);
+                    guessCount++;
+                    input = Integer.parseInt(fetch.next());
+                } else if (input > random) {
+                    System.out.println(wrongHigh);
+                    guessCount++;
+                    input = Integer.parseInt(fetch.next());
+                }
+                if (guessCount == 3) {
+                    System.out.print("Game over!");
+                    break;
+                }
+            } catch(Exception e){
+                System.out.println("Caught Exception: Which one though??");
+                //hehe.
             }
         }
     }
