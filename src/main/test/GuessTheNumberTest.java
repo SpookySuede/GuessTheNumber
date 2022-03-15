bbe7bacd9a3a806f5615ab2be860c29d8175438d
SpookySuede <spookysuede@gmail.com>
2022-03-15 12:15:22 -0400

    Adding junit testing
    incomplete upload for code review by colleague.

---
+++ src/main/test/GuessTheNumberTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+class GuessTheNumberTest {
+    GuessTheNumber guessTheNumber;
+
+    @BeforeEach
+    void setUp() {
+        guessTheNumber = new GuessTheNumber();
+    }
+
+    @AfterEach
+    void tearDown() {
+        
+    }
+
+}
